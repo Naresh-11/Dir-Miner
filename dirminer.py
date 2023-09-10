c60c3acefb930a50aa9fb83873d20aa7005d9326
White Hat <67682213+Naresh-11@users.noreply.github.com>
2023-09-10 06:14:31 +0545

    fix bugs

--- dirminer.py
+++ dirminer.py
@@ -1,9 +1,11 @@
 import argparse
-import requests
-from colorama import Fore, Style, init, Back
-import pyfiglet
+import httpx
+import asyncio
 import sys
-import random
+import os
+import pyfiglet
+from termcolor import colored
+from colorama import init
 
 init(autoreset=True)
 
@@ -12,72 +14,95 @@ def load_wordlist(wordlist_file):
         with open(wordlist_file, 'r') as file:
             return [line.strip() for line in file.readlines()]
     except FileNotFoundError:
-        print(f"{random.choice([Fore.RED, Fore.MAGENTA])}Wordlist file '{wordlist_file}' not found.{Style.RESET_ALL}")
+        print(colored(f"Wordlist file '{wordlist_file}' not found.", 'red'))
         return []
     except Exception as e:
-        print(f"{random.choice([Fore.RED, Fore.MAGENTA])}An error occurred while loading the wordlist: {e}{Style.RESET_ALL}")
+        print(colored(f"An error occurred while loading the wordlist: {e}", 'red'))
         return []
 
-def print_colored(text, text_color=None, bg_color=None, style=Style.RESET_ALL):
-    if text_color is None:
-        text_color = random.choice([Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN])
-    if bg_color is None:
-        bg_color = random.choice([Back.RED, Back.GREEN, Back.YELLOW, Back.BLUE, Back.MAGENTA, Back.CYAN])
-    
-    formatted_text = f"{bg_color}{text_color}{style}{text}{Style.RESET_ALL}"
-    print(formatted_text)
+def print_status_code(status_code):
+    if status_code == 200:
+        return colored(f"[{status_code}]", 'green', attrs=['bold'])
+    elif status_code == 404:
+        return colored(f"[{status_code}]", 'red', attrs=['bold'])
+    elif status_code == 302:
+        return colored(f"[{status_code}]", 'yellow', attrs=['bold'])
+    else:
+        return colored(f"[{status_code}]", 'white', attrs=['bold'])
 
 def print_welcome_message():
     welcome_text = pyfiglet.figlet_format("FUZZER", font="block")
-    print_colored(welcome_text, Fore.BLUE, bg_color=random.choice([Back.BLACK, Back.WHITE]))
-    print_colored("A simple dir-miner tool developed by Mr. ND", Fore.GREEN)
-    print("\n" * 2)
+    separator = '-' * 50
+    print(colored(welcome_text, 'blue'))
+    print(colored(separator, 'cyan'))
+    print(colored("A Dir-Miner tool developed by Mr. ND", 'green'))
+    print(colored(separator, 'cyan'))
 
 def print_farewell_message(author_name):
-    print("\n" * 2)
-    print_colored("Thanks for using this tool!", Fore.YELLOW)
-    print_colored(f"Author: {author_name}", Fore.CYAN)
+    separator = '-' * 50
+    print(colored(separator, 'cyan'))
+    print(colored("Thanks for using this tool!", 'yellow'))
+    print(colored(f"Author: {author_name}", 'blue'))
+    print(colored(separator, 'cyan'))
 
 def save_to_file(output_file, text):
     try:
         with open(output_file, 'a') as file:
             file.write(text + '\n')
     except Exception as e:
-        print(f"An error occurred while saving to the output file: {e}")
+        print(colored(f"An error occurred while saving to the output file: {e}", 'red'))
 
-def dirsearch(target_url, wordlist, timeout, user_agent, follow_redirects, verbose, output_file=None):
+async def dirsearch(target_url, wordlist, max_retries, timeout, user_agent, follow_redirects, output_file=None):
     try:
-        session = requests.Session()
-        session.headers['User-Agent'] = user_agent
+        async with httpx.AsyncClient() as client:
+            client.headers['User-Agent'] = user_agent
 
-        for directory in wordlist:
-            url = f"{target_url}/{directory}"
-            response = session.get(url, timeout=timeout, allow_redirects=follow_redirects)
+            for directory in wordlist:
+                url = f"{target_url}/{directory}"
+                retries = 0
 
-            status_code_color = Fore.GREEN if response.status_code == 200 else Fore.RED
-            result = f"{url} ({response.status_code})"
-            print_colored(result, status_code_color)
+                while retries <= max_retries:
+                    try:
+                        response = await client.get(url, timeout=timeout, follow_redirects=follow_redirects)
 
-            if output_file:
-                save_to_file(output_file, result)
+                        status_code = response.status_code
+                        result = f"{print_status_code(status_code)} {url}"
+                        print(result)
 
-    except requests.RequestException as e:
-        if verbose:
-            print_colored(f"Request error: {e}", Fore.RED)
-    except KeyboardInterrupt:
-        print_colored("Fuzzing interrupted by user.", Fore.YELLOW)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+                        if output_file:
+                            save_to_file(output_file, result)
+
+                        break  
+
+                    except httpx.NetworkError as e:
+                        if retries < max_retries:
+                            retries += 1
+                            print(colored(f"Network error (retrying {retries}/{max_retries}): {e}", 'yellow'))
+                            await asyncio.sleep(2 ** retries) 
+                        else:
+                            print(colored(f"Network error (max retries reached): {e}", 'red'))
+                            break
+
+                    except KeyboardInterrupt:
+                        print(colored("Fuzzing interrupted by user.", 'yellow'))
+                        return
+
+                    except Exception as e:
+                        print(colored(f"An error occurred: {e}", 'red'))
+                        break
+
+    except asyncio.CancelledError:
+        print(colored("Fuzzing task was canceled.", 'yellow'))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A simple directory search tool.")
+    parser = argparse.ArgumentParser(description="A Dir-Miner tool.")
     parser.add_argument("-u", "--url", required=True, help="Target URL to search.")
     parser.add_argument("-w", "--wordlist", help="Wordlist file containing directories to check.")
     parser.add_argument("-t", "--timeout", type=float, default=5.0, help="Timeout for HTTP requests (default: 5.0 seconds)")
     parser.add_argument("-ua", "--user-agent", default="DirectorySearchBot", help="Custom User-Agent header for HTTP requests")
     parser.add_argument("-f", "--follow-redirects", action="store_true", help="Follow HTTP redirects")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
     parser.add_argument("-o", "--output", help="Output file to save results.")
+    parser.add_argument("-r", "--retries", type=int, default=3, help="Maximum number of retries for failed requests")
 
     args = parser.parse_args()
 
@@ -88,8 +113,9 @@ if __name__ == "__main__":
     else:
         wordlist = load_wordlist(args.wordlist)
     
-    dirsearch(args.url, wordlist, args.timeout, args.user_agent, args.follow_redirects, args.verbose, args.output)
+    asyncio.run(dirsearch(args.url, wordlist, args.retries, args.timeout, args.user_agent, args.follow_redirects, args.output))
 
     print_farewell_message("Mr. ND")
 
-    sys.exit(0)
+    if args.output:
+        sys.exit(0)
